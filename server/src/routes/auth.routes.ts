0a1422596ecc0b0808bb97b3b26dd0ee831430f9
Cybrwire <jmontgomery786@gmail.com>
2026-08-21 11:29:52 +0900

    Update ichiran submodule: ignore pgdata directories

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -7,6 +7,7 @@ const router = Router();
 router.post('/register', async (req,res) => {
     const email = req.body.email;
     const pw    = req.body.pw;
+    const saltRounds = 10;
 
     if (!email || !pw){
         res.status(400).json({ error: 'Please enter both email AND password.' });
@@ -17,9 +18,15 @@ router.post('/register', async (req,res) => {
         if(result.rows.length > 0){
             res.status(409).json({ error: 'An account with this email already exists.' });
         }
+        else {
+            const hashpw = await bcrypt.hash(pw, saltRounds);
+            const newUser = await pool.query('INSERT INTO users (email,pw_hash) VALUES ($1,$2) RETURNING id, email', [email, hashpw])
+            res.status(201).json({ message:`Successfully created user: email: ${newUser.rows[0].email}, id: ${newUser.rows[0].id}`});
+        }
     } 
         catch(err){
-            
+            console.error(err);
+            res.status(500).json({error: 'error'});
     } 
 });
 
